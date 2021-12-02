3e78812217c8611d2144392f83279ac3e05c2c5d
Yoner Silva <yonerarbeyslfps.edu.co>
2021-12-01 19:50:56 -0500

    Se agrego el mapeo correspondiendte a los roles de supervisor e interventor

--- src/main/java/com/gestionObras/main/Controlador_Inter.java
+++ src/main/java/com/gestionObras/main/Controlador_Inter.java
@@ -16,6 +16,13 @@ public class Controlador_Inter {
         return "/html/Sis_Interventor_ConsuPe";
     }
     
+    @GetMapping("/Sis_Interventor_GesPed")
+    public String Sis_Interventor_GesPed(Model model, HttpSession session) {
+        Usuario usuario = (Usuario) session.getAttribute("usuario");
+        model.addAttribute("usuario", usuario);
+        return "/html/Sis_Interventor_GesPed";
+    }
+    
     @GetMapping("/Sis_Interventor_DesPedido")
     public String Sis_Interventor_DesPedido(Model model, HttpSession session) {
         Usuario usuario = (Usuario) session.getAttribute("usuario");
--- src/main/java/com/gestionObras/main/Controlador_Super.java
+++ src/main/java/com/gestionObras/main/Controlador_Super.java
@@ -17,6 +17,13 @@ public class Controlador_Super {
         return "/html/Sis_Supervisor_CargarPedido";
     }
     
+    @GetMapping("/Sis_Supervisor_GesPed")
+    public String Sis_Supervisor_GesPed(Model model, HttpSession session) {
+        Usuario usuario = (Usuario) session.getAttribute("usuario");
+        model.addAttribute("usuario", usuario);
+        return "/html/Sis_Supervisor_GesPed";
+    }
+    
     @GetMapping("/Sis_Supervisor_ConsuPe")
     public String Sis_Supervisor_ConsuPe(Model model, HttpSession session) {
         Usuario usuario = (Usuario) session.getAttribute("usuario");
