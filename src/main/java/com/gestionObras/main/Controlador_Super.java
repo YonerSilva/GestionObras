ef5f0eea716e96e2e692e6daa95c867cad650529
Yoner Silva <yonerarbeyslfps.edu.co>
2021-12-08 22:49:17 -0500

    Se corrigio PedidoServiceImpl

--- src/main/java/com/gestionObras/main/Controlador_Super.java
+++ src/main/java/com/gestionObras/main/Controlador_Super.java
@@ -1,19 +1,37 @@
 package com.gestionObras.main;
 
+import com.gestionObras.entities.Pedido;
 import com.gestionObras.entities.Usuario;
+import com.gestionObras.service.PedidoService;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Controlador_Super {
     
+    @Autowired
+    private PedidoService pedidoService;
     
     @GetMapping("/Sis_Supervisor_CargarPedido")
-    public String Sis_Supervisor_CargarPedido(Model model, HttpSession session) {
+    public String Sis_Supervisor_CargarPedido(Pedido pedido,Model model, HttpSession session) {
         Usuario usuario = (Usuario) session.getAttribute("usuario");
         model.addAttribute("usuario", usuario);
+        return "/html/Sis_Supervisor_CargarPedido";
+    }
+    
+    @PostMapping("/guardarPedido")
+    public String guardarPedido(Pedido pedido, Model model, HttpSession session){
+        Usuario usuario = (Usuario)session.getAttribute("usuario");
+        model.addAttribute("usuario", usuario);
+        try {
+            
+        } catch (Exception e) {
+            
+        }
         return "/html/Sis_Supervisor_CargarPedido";
     }
     
